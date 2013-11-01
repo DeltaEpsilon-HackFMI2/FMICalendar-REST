6b7f12f5ccf5a47a6e07f1a830739927b1ad6647
unknown <simeon@spopov-pc.comsoft.local>
2013-11-01 11:14:48 +0200

    Department Admin added

--- schedule/admin.py
+++ schedule/admin.py
@@ -2,11 +2,14 @@ from django.contrib import admin
 from schedule.models import *
 
 
+class DepartmentAdmin(admin.ModelAdmin):
+    list_display = ('name', 'id')
+
 admin.site.register(Place)
 admin.site.register(HierarchyUnit)
 admin.site.register(Block)
 admin.site.register(Subject)
-admin.site.register(Department)
+admin.site.register(Department, DepartmentAdmin)
 admin.site.register(Teacher)
 admin.site.register(Event)
 admin.site.register(Student)
