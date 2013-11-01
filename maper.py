7c514624e6b2225306d1d6703a7632d7959c920f
unknown <simeon@spopov-pc.comsoft.local>
2013-11-01 12:53:54 +0200

    Change Teacher model

--- maper.py
+++ maper.py
@@ -5,10 +5,10 @@ json_data = open('C:\\Users\\simeon.COMSOFTL\\Desktop\\HackFMI-backend\\FMICalen
 
 data = json.load(json_data)
 
-departmentsMap={'Àëã': 1, 'ÀÌ':2, 'ÂÎÈÑ':3, 'Ãåîì':4, 'ÄÓ':5, 'ÈÑ':6, 'ÈÒ':7, 'ÊÀÒ':8, 'ÊÈ':9, 'ÌËÏ':10, 'ÌÀ':11, 'ÎÌÈ':12, 'ÑÒ':13, '×ÌÀ':14, 'ËÌÌÈ~~':15}
+departmentsMap={'Àëã': 1, 'ÀÌ':2, 'ÂÎÈÑ':3, 'Ãåîì':4, 'ÄÓ':5, 'ÈÑ':6, 'ÈÒ':7, 'ÊÀÒ':8, 'ÊÈ':9, 'ÌËÏ':10, 'ÌÀ':11, 'ÎÌÈ':12, 'ÑÒ':13, '×ÌÀ':14, 'ËÌÌÈ~~':15, 'Íåîïğ':16}
 
 for x in data:
     try:
-        t = Teacher(id=x['teacherId'], name=x['teacherName'], title=x['teacherTitle'], email=x['teacherEmail'])
+        t = Teacher(id=x['teacherId'], name=x['teacherName'], title=x['teacherTitle'], email=x['teacherEmail'], position=x['teacherPosition'], department=Department.objects.get(id=departmentsMap[x['department']]))
     except UnicodeEncodeError:
         continue
