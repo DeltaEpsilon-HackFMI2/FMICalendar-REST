8f91c69e51f9e27e2b746a8c0b13baa28b252a8e
unknown <simeon@spopov-pc.comsoft.local>
2013-11-01 11:23:28 +0200

    Admin added

--- schedule/admin.py
+++ schedule/admin.py
@@ -2,15 +2,39 @@ from django.contrib import admin
 from schedule.models import *
 
 
-class DepartmentAdmin(admin.ModelAdmin):
+class PlaceAdmin(admin.ModelAdmin):
+    list_display = ('room_place', 'floor', 'id')
+
+class HierarchyUnitAdmin(admin.ModelAdmin):
+    list_display = ('value', 'type_value', 'id')
+
+class BlockAdmin(admin.ModelAdmin):
+    list_display = ('name', 'id')
+    
+class SubjectAdmin(admin.ModelAdmin):
     list_display = ('name', 'id')
 
-admin.site.register(Place)
-admin.site.register(HierarchyUnit)
-admin.site.register(Block)
-admin.site.register(Subject)
+class DepartmentAdmin(admin.ModelAdmin):
+    list_display = ('name', 'id')
+    
+class TeacherAdmin(admin.ModelAdmin):
+    list_display = ('name', 'id')
+    
+class EventAdmin(admin.ModelAdmin):
+    list_display = ('name', 'id')
+   
+class StudentAdmin(admin.ModelAdmin):
+    list_display = ('name', 'id')
+    
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('id')
+
+admin.site.register(Place,PlaceAdmin)
+admin.site.register(HierarchyUnit,HierarchyUnitAdmin)
+admin.site.register(Block,BlockAdmin)
+admin.site.register(Subject,SubjectAdmin)
 admin.site.register(Department, DepartmentAdmin)
-admin.site.register(Teacher)
-admin.site.register(Event)
-admin.site.register(Student)
-admin.site.register(Comment)
+admin.site.register(Teacher,TeacherAdmin)
+admin.site.register(Event,EventAdmin)
+admin.site.register(Student,StudentAdmin)
+admin.site.register(Comment,CommentAdmin)
